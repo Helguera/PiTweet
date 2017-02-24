3b44ac31d27bb5f559822669b06763c7fcee08ca
Javier Helguera <javier.helguera@alumnos.uva.es>
2017-02-24 10:47:01 +0100

    Se encarga de comprobar si hay tweets programados por enviar

--- hilo_tweet.java
+++ hilo_tweet.java
@@ -1,18 +1,43 @@
 package piTweet;
 
+import java.io.File;
+import java.util.Calendar;
+
+import javax.swing.JOptionPane;
+
+import twitter4j.StatusUpdate;
+import twitter4j.TwitterException;
+
 public class hilo_tweet implements Runnable {
-	
-	public hilo_tweet(){
-		
+	private Calendar actual;
+
+	public hilo_tweet() {
 	}
-	
+
 	public void run() {
-		for(int i=0; i<100;i++){
-			System.out.println(i);
-			try{
+		do {
+			for (int i = 0; i < Ventana_Main.Lista.size(); i++) {
+				System.out.println(Ventana_Main.Lista.getTweet(i).getFecha().getTime());
+				actual = Calendar.getInstance();
+				if (actual.compareTo(Ventana_Main.Lista.getTweet(i).getFecha()) >= 0) {
+					try {
+						if(Ventana_Main.Lista.getTweet(i).getImage()!=null){
+							StatusUpdate prueba = new StatusUpdate(Ventana_Main.Lista.getTweet(i).getText());
+							prueba.setMedia(new File(Ventana_Main.Lista.getTweet(i).getImage()));
+							Ventana_Main.twitter.updateStatus(prueba);
+						}else{
+							Ventana_Main.twitter.updateStatus(Ventana_Main.Lista.getTweet(i).getText());
+						}
+					} catch (TwitterException te) {
+						JOptionPane.showMessageDialog(null, "Algo ha salido mal, comprueba las Tokens", "Error", 0);
+					}
+					Ventana_Main.Lista.remove(i);
+					Ventana_Main.Lista.actualizar();
+				}
+			}
+			try {
 				Thread.sleep(1000);
-			}catch(InterruptedException we){}
-			
-		}
+			} catch (Exception o) {}
+		} while (true);
 	}
 }
